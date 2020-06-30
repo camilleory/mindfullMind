250151fa707d01e9a6ed33c8ba5bd7c134a14baa
Ruta Zemcugovaite <rutzem@gmail.com>
2020-06-30 19:10:49 +0200

    fixed npm packages

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 require('dotenv').config();
 
-const bodyParser = require('body-parser');
+const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
-
-
 const express      = require('express');
 const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-const session       = require('express-session');
-const passport      = require('passport');
 
-
-
-require('./config/passport');
 
 mongoose
-  .connect('mongodb://localhost/mindfullmind', { useNewUrlParser: true })
+  .connect('mongodb://localhost/mindfullmind', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -31,7 +24,6 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -41,38 +33,23 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
+  src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-
       
-//Session Setup
-
-const MongoStore    = require('connect-mongo')(session);
-app.use(session({
-  secret: "doesn't matter in our case", // but it's required
-  resave: false,
-  saveUninitialized: false, // don't create cookie for non-logged-in user
-  // MongoStore makes sure the user stays logged in also when the server restarts
-  store: new MongoStore({ mongooseConnection: mongoose.connection }) 
-}));
-
-
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-const auth = require('./routes/auth');
-app.use('/api', auth);
+
 
 const index = require('./routes/index');
 app.use('/', index);
