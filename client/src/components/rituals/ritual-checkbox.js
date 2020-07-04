2353995fafd60d0369966a64253c5d780d005f09
Ruta Zemcugovaite <rutzem@gmail.com>
2020-07-04 15:45:46 +0200

    finalised ritual render

--- client/src/components/rituals/ritual-checkbox.js
+++ client/src/components/rituals/ritual-checkbox.js
@@ -1,196 +1,194 @@
-import React from "react";
+import React from 'react';
 // import { Link } from 'react-router-dom'
 
 //import About from './landing/About'
-import Soundscape from "./r-sound";
-import Meditation from "./r-meditation";
-import Embodiment from "./r-body";
-import Journal from "./r-journal";
-// import RitualCheckbox from './ritual-checkbox'
+import Soundscape from './r-sound'
+import Meditation from './r-meditation'
+import Embodiment from './r-body'
+import Journal from './r-journal'
 
-class RitualChoice extends React.Component {
+
+
+
+
+class RitualCheckbox extends React.Component {
+
+
   state = {
     rituals: [],
     currentIndex: -1,
     continueButton: false,
-    deepWorkButton: false,
     showheader: true,
-    ShowDeepWork: false,
-  };
+
+
+  }
+
+
 
   onChangeMeditation = () => {
     if (this.state.rituals.includes()) {
       //this.state.rituals // remove from array
+
+
     }
     this.setState(initialState => ({
       rituals: this.state.rituals.concat("meditation")
     }));
-  };
+  }
+
+
 
   onChangeJournal = () => {
     this.setState(initialState => ({
       rituals: this.state.rituals.concat("journal")
     }));
-  };
+  }
 
   onChangeEmbodiment = () => {
     this.setState(initialState => ({
       rituals: this.state.rituals.concat("embodiment")
     }));
-  };
+  }
   onChangeSoundscape = () => {
     this.setState(initialState => ({
       rituals: this.state.rituals.concat("soundscape")
     }));
-  };
+  }
+
+
 
   // Submit Button
-  clickHandler = e => {
+  clickHandler = (e) => {
     //e.preventDefault();
     this.setState({
       currentIndex: this.state.currentIndex + 1,
-      showheader: false,
-      continueButton: true,
-    });
-    if (this.state.rituals.length - 2 === this.state.currentIndex) {
-      this.setState({
-        ShowDeepWork: true,
-        continueButton: false
-      })
-    }
-  };
+      showheader: false
+
+    })
+  }
+
 
   // Continue Button
-  nextRitualHandler = e => {
-    console.log("ritual arr", this.state.rituals)
-    console.log("current idx", this.state.currentIndex)
-
+  nextRitualHandler = (e) => {
     //e.preventDefault();
     this.setState({
       currentIndex: this.state.currentIndex + 1
-    });
-    if (this.state.rituals.length - 2 === this.state.currentIndex) {
-      this.setState({
-        ShowDeepWork: true,
-        continueButton: false
-      })
-    }
-  };
+    })
+  }
 
-  onSubmit = e => {
-    e.preventDefault();
-  };
+
+  onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+
 
 
   render() {
 
-
-
-    let currentRitual = this.state.rituals[this.state.currentIndex];
-
-
-
+    let currentRitual = this.state.rituals[this.state.currentIndex]
 
     let ritualComp;
     if (currentRitual === "soundscape") {
-      ritualComp = <Soundscape />;
+      ritualComp = <Soundscape path="/auth/soundscape" />;
+
     } else if (currentRitual === "meditation") {
-      ritualComp = <Meditation />;
+      ritualComp = <Meditation />
     } else if (currentRitual === "journal") {
-      ritualComp = <Journal />;
+      ritualComp = <Journal />
     } else if (currentRitual === "embodiment") {
-      ritualComp = <Embodiment />;
-    } else if (currentRitual === "") {
-      ritualComp = { deepWorkButton };
+      ritualComp = <Embodiment />
     }
 
-    let continueButton = (
-      <button className="btn btn-success" onClick={this.nextRitualHandler}>
-        Continue
-      </button>
-    );
 
-    let deepWorkButton = (
-      <button className="btn btn-success" onClick={this.nextRitualHandler}>
-        Deep Work
+    let continueButton = <button className="btn btn-success" onClick={this.nextRitualHandler}>
+      Continue
       </button>
-    );
+
+
     return (
       <div className="App">
-        {/* conditional component rendering in ReactJS
+
+      {/* conditional component rendering in ReactJS
       && if this.state.showheader is true, then dun the second part of the && which is the form
       */}
+        {this.state.showheader &&
 
-        {this.state.showheader && (
           <div>
             <h4>Choose your rituals before Deep Work</h4>
+            {/* 
+        <button>Meditation</button>
+        <button>Journal</button>
+        <button>Embodiment</button>
+        <button>Soundscape</button> */}
+
 
             <form onSubmit={this.onSubmit}>
               <div className="form-check">
                 <label className="form-check-label">
-                  <input
-                    type="checkbox"
+                  <input type="checkbox"
                     checked={this.state.isMeditation}
                     onChange={this.onChangeMeditation}
                     className="form-check-input"
                   />
                   Meditation
-                </label>
+            </label>
               </div>
 
               <div className="form-check">
                 <label className="form-check-label">
-                  <input
-                    type="checkbox"
+                  <input type="checkbox"
                     checked={this.state.isJournal}
                     onChange={this.onChangeJournal}
                     className="form-check-input"
                   />
                   Journal
-                </label>
+            </label>
               </div>
 
               <div className="form-check">
                 <label className="form-check-label">
-                  <input
-                    type="checkbox"
+                  <input type="checkbox"
                     checked={this.state.isSoundscape}
                     onChange={this.onChangeSoundscape}
                     className="form-check-input"
                   />
                   Soundscape
-                </label>
+            </label>
               </div>
 
               <div className="form-check">
                 <label className="form-check-label">
-                  <input
-                    type="checkbox"
+                  <input type="checkbox"
                     checked={this.state.isEmbodiment}
                     onChange={this.onChangeEmbodiment}
                     className="form-check-input"
                   />
                   Embodiment
-                </label>
+            </label>
               </div>
 
               <div className="form-group">
                 <button className="btn btn-success" onClick={this.clickHandler}>
-                  Begin
-                </button>
+                  Start
+            </button>
               </div>
             </form>
+
+
           </div>
-        )}
+
+        }
+
+
 
         {ritualComp}
-
-        {this.state.continueButton && continueButton}
-
-        {this.state.ShowDeepWork && deepWorkButton}
+        {continueButton}
       </div>
     );
   }
 }
 
-export default RitualChoice;
+
+
+export default RitualCheckbox;
